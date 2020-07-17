4543a7a91fcd61c636da4889f52a758d5b0a0ba0
AdroitMachine <anujsaxenamail@gmail.com>
2020-07-17 21:12:10 +0530

    commit

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = /*add your own api key here*/;
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
